7cf82427972fde9ffe5aaca859727b92424943e2
Tatsuki Isogami <tatsuki5820iso@gmail.com>
2020-03-16 23:40:23 +0900

    Map scene edit
    fix

---
+++ Assets/ARPass/Scripts/Scenes/Map/View/MapDemoMutator.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Linq;
+using ARPass.Avatars;
+using ARPass.Avatars.View;
+using ARPass.Utils;
+using JetBrains.Annotations;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace ARPass.Scenes.Map.View
+{
+	public sealed class MapDemoMutator : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+	{
+		[Inject, UsedImplicitly]
+		IMapClient _client;
+		
+		[SerializeField]
+		AbstractMap _map;
+
+		[SerializeField]
+		Vector2d _lonLatOrigin;
+
+		[SerializeField, Range(1, 22)]
+		int _zoom = 18;
+
+		[SerializeField, Range(0, 1)]
+		float _speed = 0.2f;
+
+		[SerializeField]
+		AvatarAnimator _animator;
+		
+		Vector2 _touchOrigin;
+
+		Vector2d _current;
+
+		string _state;
+
+		void Start()
+		{
+			_map.Initialize(_lonLatOrigin, _zoom);
+			_current = _lonLatOrigin;
+			GUIField.Subscribe(this, 0, _onGui);
+		}
+		
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			_touchOrigin = eventData.position;
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			var diff = (eventData.position - _touchOrigin) * _speed;
+			_current = _lonLatOrigin - new Vector2d(diff.y, diff.x);
+			_animator.Animate(-diff);
+			_map.UpdateMap(_current);
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			_lonLatOrigin = _current;
+			_animator.Animate(Vector2.zero);
+		}
+
+		void _onGui()
+		{
+			if (GUILayout.Button("Save", GUILayout.Height(40)))
+			{
+				SavePosition().Away();
+			}
+			GUILayout.Label(_state);
+			GUILayout.Label($"Latitude: {_current.x}");
+			GUILayout.Label($"Longitude: {_current.y}");
+		}
+
+		async UniTask SavePosition()
+		{
+			_state = "Saving position...";
+			await _client
+				.SavePosition(new Vector2((float)_current.x, (float)_current.y));
+			_state = "Saved!";
+		}
+	}
+}
